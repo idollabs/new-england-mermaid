f8caf877c5622c95c439d8bcb9fad3e5effed816
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-27 16:26:07 -0600

    addition of hamburger menu but still needs editing

--- index.js
+++ index.js
@@ -27,3 +27,13 @@ gridChild.on('mouseenter', function () {
 gridChild.on('mouseleave', function () {
   handleBlogInfo(0);
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
